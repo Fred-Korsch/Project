5612dce1f150243a4079fcabdf7cd95839327c29
Frederico Korsch <korsch02@gmail.com>
2017-06-30 17:48:06 -0300

    Scaffold customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630204522) do
+ActiveRecord::Schema.define(version: 20170630204710) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.date "birthday"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
