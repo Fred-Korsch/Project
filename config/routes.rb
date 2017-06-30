776e7d60f16a942b8cc9a5c4990ab24fe8d525d4
Frederico Korsch <korsch02@gmail.com>
2017-06-30 17:52:23 -0300

    Fix user model of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :customers
-  devise_for :models
+  devise_for :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    authenticated :user do
+      root 'customers#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
